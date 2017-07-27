b5693b68d449c298d2935a310410eb3290fc26ce
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-07-26 22:08:08 -0400

    added logic to check for duplicate items added to the cart (case-insensitive)

--- basics/shopping-cart/shoppingCart.js
+++ basics/shopping-cart/shoppingCart.js
@@ -3,15 +3,20 @@ var app = angular.module("ShoppingCart", []);
 
 app.controller("ShoppingCartController", function ($scope) {
     $scope.products = ["Milk", "Bread", "Cheese"];
+    $scope.lowercaseProducts = [];
 
     $scope.addItem = function () {
+        // Check the existing products
+        $scope.products.forEach(function(product) {
+            $scope.lowercaseProducts.push(product.toLowerCase());
+        });
         $scope.errorText = "";
 
         if (!$scope.addItem) {
             return;
         }
 
-        if ($scope.products.indexOf($scope.item) == -1) {
+        if ($scope.lowercaseProducts.indexOf($scope.item.toLowerCase()) == -1) {
             $scope.products.push($scope.item);
             $scope.item = ''; // Clear the input
         } else {

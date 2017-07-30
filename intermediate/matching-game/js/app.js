64371a873ad1f95e2dde79592817c4eb67e718ab
usman-tahir <tahir.usman.ali94@outlook.com>
2017-07-30 17:14:55 -0400

    added the implementation for the helper functions

--- intermediate/matching-game/js/app.js
+++ intermediate/matching-game/js/app.js
@@ -1,7 +1,98 @@
 (function () {
   "use strict";
 
-  var app = angular.module("MatchingGame", []);
+  var app = angular.module("MatchingGame", ['ngAnimate']);
+
+  // Constants to use in the game
+  var constants,
+      currentSessionOpen = false,
+      previousCard = null,
+      numberOfPairs = 0;
+
+  constants = new (function() {
+    var rows = 3,
+        columns = 6,
+        numberOfMatches = (rows * columns) / 2;
+
+    this.getRows = function() {
+      return rows;
+    };
+
+    this.getColumns = function() {
+      return columns;
+    };
+
+    this.getNumberOfMatches = function() {
+      return numberOfMatches;
+    };
+  })();
+
+  function createDeck() {
+    var rows = constants.getRows(),
+        columns = constants.getColumns(),
+        key = createRandom(),
+        deck = {},
+        i,
+        j;
+    deck.rows = [];
+
+    // Create each row
+    for (i = 0; i < rows; i += 1) {
+      var row = {};
+      row.cards = [];
+
+      for (j = 0; j < columns; j += 1) {
+        var card = {};
+        card.isFaceUp = false;
+        card.item = key.pop();
+        row.cards.push(card);
+      }
+      deck.rows.push(row);
+    }
+    return deck;
+  }
+
+  function removeByIndex(array, index) {
+    array.splice(index, 1);
+  }
+
+  function insertByIndex(array, index, item) {
+    array.splice(index, 0, item);
+  }
+
+  // Create an array of items that simulates a matching scheme
+  function createRandom() {
+    var matches = constants.getNumberOfMatches(),
+        pool = [],
+        answers = [],
+        letters = [
+          'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+          'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+        ],
+        items = letters,
+        i,
+        j;
+
+    for (i = 0; i < (matches * 2), i += 1) {
+      pool.push(i);
+    }
+
+    // Generate an array with the random items
+    for (j = 0; j < matches; j += 1) {
+      var randomLetter = Math.floor((Math.random() * items.length)),
+          letter = items[randomLetter],
+          randomPool = Math.floor((Math.random() * pool.length));
+      removeByIndex(items, randomLetter);
+      insertByIndex(answers, pool[randomPool], letter);
+      removeByIndex(pool, randomPool);
+
+      // Second placement
+      randomPool = Math.floor((Math.random() * pool.length));
+      insertByIndex(answers, pool[randomPool], letter);
+      removeByIndex(pool, randomPool);
+    }
+    return answers;
+  }
 
   app.controller("CardController", function ($scope, $timeout) {
     $scope.deck = createDeck();
